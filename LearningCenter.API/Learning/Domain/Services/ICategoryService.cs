2c358ad232169c744e81d36a89c687c2c5c3a6c0
Angel Velasquez <pcisavel@upc.edu.pe>
2022-05-17 14:02:54 -0500

    feat(categories): Implemented Service Interface.

---
+++ LearningCenter.API/Learning/Domain/Services/ICategoryService.cs
@@ -0,0 +1,12 @@
+using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Learning.Domain.Services.Communication;
+
+namespace LearningCenter.API.Learning.Domain.Services;
+
+public interface ICategoryService
+{
+    Task<IEnumerable<Category>> ListAsync();
+    Task<CategoryResponse> SaveAsync(Category category);
+    Task<CategoryResponse> UpdateAsync(int id, Category category);
+    Task<CategoryResponse> DeleteAsync(int id);
+}
