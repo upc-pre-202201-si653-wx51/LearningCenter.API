5b3842b137f8e8de198347c6779474c38f7ea7a2
Angel Velasquez <pcisavel@upc.edu.pe>
2022-05-17 14:39:12 -0500

    feat(categories): Added Incoming Mapping configuration.

---
+++ LearningCenter.API/Learning/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Learning.Resources;
+
+namespace LearningCenter.API.Learning.Mapping;
+
+public class ResourceToModelProfile : Profile
+{
+    public ResourceToModelProfile()
+    {
+        CreateMap<SaveCategoryResource, Category>();
+    }
+}
---
+++ LearningCenter.API/Learning/Resources/SaveCategoryResource.cs
@@ -0,0 +1,6 @@
+namespace LearningCenter.API.Learning.Resources;
+
+public class SaveCategoryResource
+{
+    public string Name { get; set; }
+}
