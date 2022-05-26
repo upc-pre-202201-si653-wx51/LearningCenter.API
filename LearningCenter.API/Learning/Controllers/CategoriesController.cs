4a2f9af2897ee3990aafae662687cf75016d11f7
Angel Velasquez <pcisavel@upc.edu.pe>
2022-05-26 14:48:22 -0500

    feat(categories): Added inbound service.

--- LearningCenter.API/Learning/Controllers/CategoriesController.cs
+++ LearningCenter.API/Learning/Controllers/CategoriesController.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using LearningCenter.API.Learning.Domain.Models;
 using LearningCenter.API.Learning.Domain.Services;
 using LearningCenter.API.Learning.Resources;
+using LearningCenter.API.Shared.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LearningCenter.API.Learning.Controllers;
@@ -26,5 +27,53 @@ public class CategoriesController : ControllerBase
         var resources = _mapper.Map<IEnumerable<Category>, IEnumerable<CategoryResource>>(categories);
 
         return resources;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] SaveCategoryResource resource)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState.GetErrorMessages());
+
+        var category = _mapper.Map<SaveCategoryResource, Category>(resource);
+
+        var result = await _categoryService.SaveAsync(category);
+
+        if (!result.Success)
+            return BadRequest(result.Message);
+
+        var categoryResource = _mapper.Map<Category, CategoryResource>(result.Resource);
+
+        return Ok(categoryResource);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync(int id, [FromBody] SaveCategoryResource resource)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState.GetErrorMessages());
+        
+        var category = _mapper.Map<SaveCategoryResource, Category>(resource);
+        var result = await _categoryService.UpdateAsync(id, category);
+        
+        if (!result.Success)
+            return BadRequest(result.Message);
+
+        var categoryResource = _mapper.Map<Category, CategoryResource>(result.Resource);
+
+        return Ok(categoryResource);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var result = await _categoryService.DeleteAsync(id);
+
+        if (!result.Success)
+            return BadRequest(result.Message);
+        
+        var categoryResource = _mapper.Map<Category, CategoryResource>(result.Resource);
+
+        return Ok(categoryResource);
     }
 }
