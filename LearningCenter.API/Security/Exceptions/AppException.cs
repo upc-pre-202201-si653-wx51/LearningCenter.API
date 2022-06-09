466893684dcb9cb9fd696cb98b6ee3e4619bf01f
Angel Velasquez <pcisavel@upc.edu.pe>
2022-06-09 14:41:10 -0500

    feat(security): Added Custom Exception.

---
+++ LearningCenter.API/Security/Exceptions/AppException.cs
@@ -0,0 +1,18 @@
+using System.Globalization;
+
+namespace LearningCenter.API.Security.Exceptions;
+
+public class AppException : Exception
+{
+    public AppException() : base() { }
+
+
+    public AppException(string? message) : base(message)
+    {
+    }
+
+    public AppException(string message, params object[] args)
+        : base(String.Format(CultureInfo.CurrentCulture, message, args))
+    {
+    }
+}
