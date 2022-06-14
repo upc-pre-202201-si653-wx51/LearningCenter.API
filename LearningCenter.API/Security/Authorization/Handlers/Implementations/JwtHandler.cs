54348a1f42edb39f6cdb9b7003da5a2337085f12
Angel Velasquez <pcisavel@upc.edu.pe>
2022-06-14 13:32:50 -0500

    feat(security): Implemented JWT Creation and Validation.

--- LearningCenter.API/Security/Authorization/Handlers/Implementations/JwtHandler.cs
+++ LearningCenter.API/Security/Authorization/Handlers/Implementations/JwtHandler.cs
@@ -1,7 +1,11 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using LearningCenter.API.Security.Authorization.Handlers.Interfaces;
 using LearningCenter.API.Security.Authorization.Settings;
 using LearningCenter.API.Security.Domain.Models;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace LearningCenter.API.Security.Authorization.Handlers.Implementations;
 
@@ -16,11 +20,62 @@ public class JwtHandler : IJwtHandler
 
     public string GenerateToken(User user)
     {
-        throw new NotImplementedException();
+        // Generate Token for a valid period of 7 days.
+
+        var secret = _appSettings.Secret;
+        var key = Encoding.ASCII.GetBytes(secret);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Sid, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username)
+            }),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha512Signature)
+        };
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
     }
 
     public int? ValidateToken(string token)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(token))
+            return null;
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+        
+        // Execute Token Validation
+
+        try
+        {
+            tokenHandler.ValidateToken(token, new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                // Expiration with no delay
+                ClockSkew = TimeSpan.Zero
+            }, out var validatedToken);
+
+            var jwtToken = (JwtSecurityToken)validatedToken;
+            var userId = int.Parse(jwtToken.Claims.First(
+                claim => claim.Type == ClaimTypes.Sid).Value);
+
+            return userId;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return null;
+        }
+
     }
 }
